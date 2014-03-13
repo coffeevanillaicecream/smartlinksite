a5f88012ebe21b89f154a37b37be64312ee22d9f
ChibiAlmond <a.sato@gmail.com>
2014-03-13 16:37:07 -0700

    added about us page

---
+++ application/views/aboutus.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Basic Company Template for Bootstrap 3</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php echo base_url().'css/bootstrap.css'; ?>" rel="stylesheet">
+
+    <!-- Custom CSS for the 'Business Frontpage' Template -->
+    <link href="<?php echo base_url().'css/business-frontpage.css'; ?>" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Smart Link</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#about">About</a>
+                    </li>
+                    <li><a href="#services">Services</a>
+                    </li>
+                    <li><a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="business-header">
+
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- The background image is set in the custom CSS -->
+                    <h1 class="tagline">An Informative Tagline Goes Here</h1>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <hr>
+
+        <div class="row">
+            <div class="col-lg-8 col-sm-8">
+                <h2>About Us</h2>
+                <p>The company started as construction firm, committed to provide its ever growing “SmartLinked” rural communities to total services through information infrastructure. It launched its  core products and services by bundling and converging the building construction with structured wiring, security systems and automations, and Wireless Internet Connectivity. It assured their clients and patrons of an exclusive stand-alone feature in their insatiable need for ever current and logarithmically growing information and data exchange network in real-time, for record keeping in mirror sites and their convenient access through wireless network infrastructure. It has started a satellite wireless  mesh network  in three-last mile in the Northern tip of the Philippines and successfully launched itself as emerging  wireless internet access provider.</p>
+                <p>Now it is fast bundling its services with the offer of security cameras linked through satellite  internet and the many more electronic devices and  telecommunication systems for the total services to its clients and target market. </p>
+                <p>SMARTLINK envisioned a dominant  leadership in the wireless internet connectivity with convergence of security features and network systems to serve best their primary target markets –which are the last mile SME’s, farmer-growers, rural entrepreneurs, students, LGU’s and empowering them for competitiveness leveling with their urban counterparts –as far as their products and core competencies can be marketed & made accessible to international markets while  keeping them abreast on local collaborations for services/products volume & quality enhancements  to attain global competitiveness. </p>
+
+                </p>
+                <p><a class="btn btn-default btn-large" href="#">Call to action &raquo;</a>
+                </p>
+            </div>
+            <div class="col-lg-4 col-sm-4">
+                <h2>Contact Us</h2>
+                <address>
+                    <strong>SMARTLINK TECHNOLOGIES AND BUSINESS SOLUTIONS CO.</strong>
+                    <br>507 Prince John Condo 291 P. Tuazon corner 18th Ave. San. Roque, QC.
+                    <br>Manila, Philippines
+                    <br>
+                </address>
+                <address>
+                    <abbr title="Tel./fax">Phone Number & Fax:</abbr> (02)5175989 & (02)703-9685
+                    <br>
+                    <abbr title="Mobile">Mobile:</abbr> 0908-1567581   
+                    <br>
+                    <abbr title="Email">Email:</abbr> <a href="mailto:#">greatwaylink@gmail.com</a>
+                </address>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="row">
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive text-center" src="http://placehold.it/300x300">
+                <h2>Marketing Box #1</h2>
+                <p>These marketing boxes are a great place to put some information. These can contain summaries of what the company does, promotional information, or anything else that is relevant to the company. These will usually be below-the-fold.</p>
+            </div>
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive" src="http://placehold.it/300x300">
+                <h2>Marketing Box #2</h2>
+                <p>The images are set to be circular and responsive. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+            </div>
+            <div class="col-sm-4">
+                <img class="img-circle img-responsive" src="http://placehold.it/300x300">
+                <h2>Marketing Box #3</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Company 2013</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /container -->
+
+
+    <!-- JavaScript -->
+    <script src="js/jquery-1.10.2.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
